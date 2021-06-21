641d63e1e4f27a999087db56bd0fd4f63e098472
allynsweet <allyn.sweet@uconn.edu>
2021-06-21 15:04:06 -0400

    refactor code and improve readability

--- parseMag.js
+++ parseMag.js
@@ -1,50 +1,60 @@
 import usb from 'usb';
-//lists all devices
-//console.log(usb.getDeviceList());
+// lists all devices
+// console.log(usb.getDeviceList());
 
-//specify port with vendorid + productid
-//console.log(usb.findByIds(1060,9492));
+// specify port with vendorid + productid
+// console.log(usb.findByIds(1060,9492));
 
-let stream = usb.findByIds(1659, 8963);
-stream.open(); 
+// Define constants for vendorID and productID, look up "magic numbers" for more details.
+const VENDOR_ID = 1659;
+const PRODUCT_ID = 8963;
+const stream = usb.findByIds(VENDOR_ID, PRODUCT_ID);
+const allUSBDevices = usb.getDeviceList();
 
-let endpoints = stream.interface(0).endpoints
-console.log(endpoints)
-usb.Interface.claim()
-    usb.InEndpoint = endpoints[0],
-    usb.OutEndpoint = endpoints[1];
-usb.InEndpoint.trasferType = 2
-usb.InEndpoint.startPoll(1, 64)
-usb.InEndpoint.transfer(64, function (error, data) {
-    if (!error) {
-        console.log(data);
-    } else {
-        console.log(error);
-    }
+// Ensure the stream is defined to prevent unwanted "not found" errors.
+if (!stream) {
+  const foundDevice = allUSBDevices[0] || undefined;
+  // If there is an error, print that message along with the idProduct and idVendor of a found device.
+  const foundDeviceMessage = !!foundDevice
+    ? `Did you mean PID ${foundDevice.deviceDescriptor.idProduct} and VID ${foundDevice.deviceDescriptor.idVendor}?`
+    : 'There are currently no devices plugged into this computer.';
+  throw new Error(
+    `USB device with vendor ID of ${VENDOR_ID} and product ID of ${PRODUCT_ID} could not be found. ${foundDeviceMessage}`
+  );
+}
+// Now that we know stream is defined, we can use it.
+stream.open();
+
+const { endpoints } = stream.interface(0);
+usb.Interface.claim();
+[usb.InEndpoint, usb.OutEndpoint] = endpoints;
+usb.InEndpoint.transferType = 2;
+usb.InEndpoint.startPoll(1, 64);
+usb.InEndpoint.transfer(64, (error, data) => {
+  if (error) {
+    throw error;
+  }
+  console.log(data);
 });
 
-usb.InEndpoint.on('data', function (data) {
-    console.log(data);
+usb.InEndpoint.on('data', (data) => {
+  console.log(data);
 });
 
-usb.InEndpoint.on('error', function (error) {
-    console.log(error);
+usb.InEndpoint.on('error', (error) => {
+  console.log(error);
 });
 
+// let string = /\d{5}\.\d{3}[\s,]\d{4}[\s,]\d{4}[\s,]\d{4}/;
+// console.log(string.test("12345.123,1234,1858,1234"))
 
-
-//let string = /\d{5}\.\d{3}[\s,]\d{4}[\s,]\d{4}[\s,]\d{4}/;
-//console.log(string.test("12345.123,1234,1858,1234"))
-
-
-
-//function parseMaggy(datastream) {
+// function parseMaggy(datastream) {
 //    if (!datastream) {
 //        badString.push(datastream)
 //    } else if (string === true) {
 //        correctString.push(datastream)
-   // }     
-//}
+// }
+// }
 
-//parseMaggy(string);
-//console.log(correctString);
+// parseMaggy(string);
+// console.log(correctString);
