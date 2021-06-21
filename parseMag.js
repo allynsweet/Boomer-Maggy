274422f95eb000b7b7d4fe6d1915311fc72ca1eb
allynsweet <allyn.sweet@uconn.edu>
2021-06-21 16:17:20 -0400

    receive data from endpoint

--- parseMag.js
+++ parseMag.js
@@ -1,4 +1,4 @@
-import usb from 'usb';
+import usb, { InEndpoint, OutEndpoint } from 'usb';
 // lists all devices
 // console.log(usb.getDeviceList());
 
@@ -8,11 +8,11 @@ import usb from 'usb';
 // Define constants for vendorID and productID, look up "magic numbers" for more details.
 const VENDOR_ID = 1659;
 const PRODUCT_ID = 8963;
-const stream = usb.findByIds(VENDOR_ID, PRODUCT_ID);
+const device = usb.findByIds(VENDOR_ID, PRODUCT_ID);
 const allUSBDevices = usb.getDeviceList();
 
 // Ensure the stream is defined to prevent unwanted "not found" errors.
-if (!stream) {
+if (!device) {
   const foundDevice = allUSBDevices[0] || undefined;
   // If there is an error, print that message along with the idProduct and idVendor of a found device.
   const foundDeviceMessage = !!foundDevice
@@ -22,28 +22,27 @@ if (!stream) {
     `USB device with vendor ID of ${VENDOR_ID} and product ID of ${PRODUCT_ID} could not be found. ${foundDeviceMessage}`
   );
 }
-// Now that we know stream is defined, we can use it.
-stream.open();
-
-const { endpoints } = stream.interface(0);
-usb.Interface.claim();
-[usb.InEndpoint, usb.OutEndpoint] = endpoints;
-usb.InEndpoint.transferType = 2;
-usb.InEndpoint.startPoll(1, 64);
-usb.InEndpoint.transfer(64, (error, data) => {
-  if (error) {
-    throw error;
-  }
-  console.log(data);
+// Now that we know device is defined, we can use it.
+device.open(true);
+const deviceInterface = device.interfaces[0];
+if (!deviceInterface) {
+  throw new Error('USB interface cannot be claimed because it is undefined.');
+}
+// If device interface is defined, we can claim it and start polling.
+deviceInterface.claim();
+const inEndpoint = deviceInterface.endpoints[1];
+if (!inEndpoint) {
+  throw new Error('Interface must be defined.');
+}
+inEndpoint.startPoll(1, 64);
+// When new data comes in a data event will be fired on the receive endpoint
+inEndpoint.on('data', (dataBuf) => {
+  // Convert buffer to array
+  let dataArr = Array.prototype.slice.call(new Uint8Array(dataBuf, 0, 8));
+  console.log(`Data Received: ${JSON.stringify(dataArr)}`);
 });
+console.log('Listening to USB for data.');
 
-usb.InEndpoint.on('data', (data) => {
-  console.log(data);
-});
-
-usb.InEndpoint.on('error', (error) => {
-  console.log(error);
-});
 
 // let string = /\d{5}\.\d{3}[\s,]\d{4}[\s,]\d{4}[\s,]\d{4}/;
 // console.log(string.test("12345.123,1234,1858,1234"))
